29aaba34d81bf1e58b0a9781c573e670753c891e
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-01-12 19:51:26 +0200

    add timestamp to exp name

--- optimize.py
+++ optimize.py
@@ -2,23 +2,27 @@ import optuna
 import Main
 import yaml
 from yaml.loader import SafeLoader
+from datetime import datetime
+
 
 
 
 def objective(trial):
-
+    curr_dt = datetime.now()
+    timestamp = int(round(curr_dt.timestamp()))
+    exp_name = 'test_naive_2_experts_' + str(timestamp)
     expert_coeff = trial.suggest_float('expert_coeff', 1e-10, 0.1, log=True)
     n_experts = trial.suggest_categorical("n_experts", ['1', '2', '4', '8', '16'])
 
     with open('configuration.yaml', 'r') as f:
         config = yaml.load(f, Loader=SafeLoader)
     data, model, train_loss, train_acc, test_loss, test_acc =\
-        Main.main(n_epochs=2000,
+        Main.main(n_epochs=1,
             experts_coeff=expert_coeff,
             dataset_name='mnist',
-            n_experts=int(n_experts),
+            n_experts=2,
             expert_type='naive_fc',
-            experiment_name='test_naive',
+            experiment_name=exp_name,
             load_model=None
             )
     return max(test_acc)
