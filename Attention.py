cb52e34a13e28ae9bddc2933397e482872f76ed4
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-02-10 21:15:43 +0200

    .

--- Attention.py
+++ Attention.py
@@ -11,8 +11,12 @@ class AdditiveAttention(nn.Module):
         self.key_proj = nn.Linear(input_dim, input_dim, bias=False)
         self.bias = nn.Parameter(torch.rand(input_dim).uniform_(-0.1, 0.1))
         self.score_proj = nn.Linear(input_dim, 1)
+        self.warm_up = 0
 
     def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         score = self.score_proj(torch.tanh(self.key_proj(key) + self.query_proj(query) + self.bias)).squeeze(1)
         attn = F.softmax(score, dim=0)
+        if self.warm_up < 50:
+            self.warm_up += 1
+            attn = torch.full_like(attn, 0.5)
         return attn
