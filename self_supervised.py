546c9bc9fd6a04213aa30f25096ed83194c4c967
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-04-23 18:07:38 +0300

    .

--- self_supervised.py
+++ self_supervised.py
@@ -34,10 +34,11 @@ def label_samples(model, unlabeled_trainloader, labeled_trainloader, th=0.5):
     model.train()
 
 def fit(dataset, model):
-    logger = get_logger(setup['experiment_name'])
-    labeled_indexes, unlabeled_indexes = initial_split(train_loader=dataset['train_loader'])
+    # logger = get_logger(setup['experiment_name'])
+    # labeled_indexes, unlabeled_indexes = initial_split(train_loader=dataset['train_loader'])
     while len(unlabeled_indexes) != 0:
-        logger.info(f'unlabeled samples: {len(unlabeled_indexes)}')
+        # logger.info(f'unlabeled samples: {len(unlabeled_indexes)}')
+        print(f'unlabeled samples: {len(unlabeled_indexes)}')
         labeled_sampler = SubsetRandomSampler(labeled_indexes)
         labeled_trainloader = torch.utils.data.DataLoader(dataset['train_loader'].dataset, batch_size=64, sampler=labeled_sampler)
 
