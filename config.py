9a674a4cff02a5bcbed6952ed987766724eec55e
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-04-28 15:02:05 +0300

    .

--- config.py
+++ config.py
@@ -63,13 +63,13 @@ setup = {'n_epochs': 200,
          'expert_type': 'resnet18',
          'dataset_name': 'cifar10',
          'n_experts': 2,
-         'experiment_name': 'ssl_2_experts_1000_labels_20',
+         'experiment_name': 'ssl_2_experts_1000_labels_21',
          'model_checkpoint_path': None,
          'model_eval_path': None,
          'evaluate': False,
-         'ssl': 1_000,
+         'ssl': 10_000,
          'ssl_lr': 0.01,
-         'ssl_th': 0.3
+         'ssl_th': 0.5
          }
 
 
